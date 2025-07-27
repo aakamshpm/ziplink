d9a6f366c22a0d479a22a2389d543a5f2c314eef
Aakamsh PM <aakamshpm@gmail.com>
2025-07-27 22:25:23 +0530

    imported configs & modules into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import appConfig from './config/app.config';
+import { PrismaModule } from './prisma/prisma.module';
+import { CounterModule } from './counter/counter.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig],
+    }),
+    PrismaModule,
+    CounterModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
