0f11c544f6330e7afb89b9cf20ad8d4aa2449d2d
Aakamsh PM <aakamshpm@gmail.com>
2025-07-31 22:08:23 +0530

    redic cache module

---
+++ src/cache/cache.module.ts
@@ -0,0 +1,30 @@
+import KeyvRedis from '@keyv/redis';
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import Keyv from 'keyv';
+import { CacheService } from './cache.service';
+
+@Global()
+@Module({
+  imports: [
+    NestCacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const redisConfig = configService.get('redis.cache');
+
+        return {
+          store: new Keyv({
+            store: new KeyvRedis(redisConfig),
+            namespace: 'cache',
+          }),
+          ttl: configService.get('redis.ttl.urls') * 1000,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [CacheService],
+  exports: [CacheService],
+})
+export class CacheModule {}
