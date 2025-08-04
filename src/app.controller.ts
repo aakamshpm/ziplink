0ac658188aa4cd94d8e147a731b963468e81077c
Aakamsh PM <aakamshpm@gmail.com>
2025-08-05 00:36:30 +0530

    removed test endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { CacheService } from './cache/cache.service';
+import { Controller } from '@nestjs/common';
 
-@Controller('debug')
-export class AppController {
-  constructor(private readonly cacheService: CacheService) {}
-
-  @Get('redis/test/:shortCode')
-  async testRedisInstances(@Param('shortCode') shortCode: string) {
-    // Test URL cache (should go to port 6379)
-    await this.cacheService.setUrl(shortCode, 'https://test.com');
-    const cachedUrl = await this.cacheService.getUrl(shortCode);
-
-    // Test click increment (should go to port 6380)
-    const clickCount = await this.cacheService.incrementClickCount(shortCode);
-
-    return {
-      shortCode,
-      urlCache: {
-        stored: !!cachedUrl,
-        data: cachedUrl,
-        message: 'Should be in Redis port 6379',
-      },
-      analytics: {
-        clickCount,
-        message: 'Should be in Redis port 6380',
-      },
-    };
-  }
-}
+@Controller()
+export class AppController {}
