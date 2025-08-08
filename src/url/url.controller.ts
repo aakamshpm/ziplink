df8a52b6b8c44a19ae3fee1fe0cca6367f1606ba
Aakamsh PM <aakamshpm@gmail.com>
2025-08-08 15:34:23 +0530

    feat: enhance UrlController with Swagger API documentation

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -14,7 +14,9 @@ import {
 import { UrlService } from './url.service';
 import { CreateUrlDto, UrlResponseDto, UrlStatsDto } from './create-url.dto';
 import { RateLimit, RateLimitGuard } from 'src/rate-limit/rate-limit.guard';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('urls')
 @Controller('api/urls')
 @UseGuards(RateLimitGuard)
 @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
@@ -22,6 +24,14 @@ export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
   @Post('shorten')
+  @ApiOperation({ summary: 'Shorten a URL' })
+  @ApiResponse({
+    status: 201,
+    description: 'URL shortened',
+    type: UrlResponseDto,
+  })
+  @ApiResponse({ status: 400, description: 'Invalid URL provided' })
+  @ApiResponse({ status: 429, description: 'Too many URL shortening requests' })
   @HttpCode(HttpStatus.CREATED)
   @RateLimit({
     limit: 10,
@@ -43,6 +53,18 @@ export class UrlController {
   }
 
   @Get('stats/:shortCode')
+  @ApiOperation({ summary: 'Get statistics for a short URL' })
+  @ApiParam({
+    name: 'shortCode',
+    description: 'Short code of the URL',
+    example: 'abc123',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Url Stats fetched',
+    type: UrlStatsDto,
+  })
+  @ApiResponse({ status: 404, description: 'Short code not found' })
   async getUrlStats(@Param('shortCode') shortCode: string): Promise<{
     success: true;
     data: UrlStatsDto;
@@ -58,6 +80,14 @@ export class UrlController {
   }
 
   @Delete('delete/:shortCode')
+  @ApiOperation({ summary: 'Delete a short URL' })
+  @ApiParam({
+    name: 'shortCode',
+    description: 'Short code to delete',
+    example: 'abc123',
+  })
+  @ApiResponse({ status: 204, description: 'Short code deleted' })
+  @ApiResponse({ status: 404, description: 'Short code not found' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteUrl(@Param('shortCode') shortCode: string): Promise<{
     success: true;
